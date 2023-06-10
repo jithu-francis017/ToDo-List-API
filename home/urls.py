db2be0e737e6c262075e51b30e2a9c3c37d9ee2b
JITHU FRANCIS <117467296+jithu-francis017@users.noreply.github.com>
2023-06-10 21:06:39 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -11,9 +11,9 @@ router.register(r'todos', TodoViewSet)
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
     path('register/', RegisterPage.as_view(), name='register'),
-    path('', TaskList.as_view(), name='tasks'),
+    path('dashboard/', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
